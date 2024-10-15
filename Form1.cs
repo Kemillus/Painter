35b8efecfe5e56afe312a9a242602b91d12fa02a
Kemillus <b.a.a.ss@mail.ru>
2024-10-15 21:02:38 +0300

    v1.1

--- Form1.cs
+++ Form1.cs
@@ -12,128 +12,120 @@ namespace Painter
 {
     public partial class Form1 : Form
     {
-        private const int WIDTH = 1200;
-        private const int HEIGHT = 800;
-        private const int FIGURE_X = 600;
-        private const int FIGURE_Y = 350;
-        private const int FIGURE_SIZE = 300;
-
-        private Controll[] controls = new Controll[5];
-        private int controlIndex = 0;
+        private Color color = Color.White;
+        private Color[] colors;
+        private Rectangle rect = new Rectangle(250, 250, 150, 150);
+        private Bitmap canvas;
 
         public Form1()
         {
-            this.Width = WIDTH;
-            this.Height = HEIGHT;
-            this.Text = "Graphics";
-
-            this.Paint += new PaintEventHandler(drawFigure);
-
-            controls[0] = new Controll { Left = 0, Top = 0, Right = 100, Bottom = 100 };
-            controls[1] = new Controll { Left = 0, Top = 700, Right = 100, Bottom = 800 };
-            controls[2] = new Controll { Left = 900, Top = 700, Right = 1000, Bottom = 800 };
-            controls[3] = new Controll { Left = 1000, Top = 700, Right = 1100, Bottom = 800 };
-            controls[4] = new Controll { Left = 1100, Top = 700, Right = 1200, Bottom = 800 };
-
+            this.Width = 700;
+            this.Height = 500;
+            colors = new Color[] { Color.Red, Color.Green, Color.Gray, Color.Pink, Color.Violet };
+            Paint += new PaintEventHandler(Painter);
+            MouseClick += new MouseEventHandler(OnMouseClick);
+            CreateButtons();
+            canvas = new Bitmap(this.Width, this.Height);
+            this.BackgroundImage = new Bitmap("D:\\Veronika\\Painter\\Canvas2.png");
+            Graphics g = Graphics.FromImage(canvas);
+            g.FillRectangle(Brushes.White, rect);
+            g.DrawRectangle(Pens.Black, rect);
+            g.FillEllipse(Brushes.White, rect);
+            g.DrawEllipse(Pens.Black, rect);
+            g.DrawLine(Pens.Black, rect.Left, rect.Top + rect.Top / 2, rect.Right, rect.Top + rect.Top / 2);
             //createControl(0, 0, 0, "D:\\Veronika\\Painter\\canvas.bmp");
             //createControl(1, 0, 700, "D:\\Veronika\\Painter\\palette.bmp");
             //createControl(2, 900, 700, "D:\\Veronika\\Painter\\clear.bmp");
             //createControl(3, 1000, 700, "D:\\Veronika\\Painter\\save.bmp");
             //createControl(4, 1100, 700, "D:\\Veronika\\Painter\\exit.bmp");
-            
-            this.BackgroundImage = new Bitmap("D:\\Veronika\\Painter\\canvas.bmp");
-            CreateImage(0, 0, 0, "D:\\Veronika\\Painter\\palette.bmp");
-            this.Update();
         }
 
-        private void CreateImage(int i,int left, int top, string fileName)
+        private void OnMouseClick(object sender, MouseEventArgs e)
         {
-            PictureBox pictureBox = new PictureBox();
-            pictureBox.Image = new Bitmap(fileName);
-            this.Controls.Add(pictureBox);
+            if (rect.Contains(e.Location))
+            {
+                FlooeFill(canvas, e.Location.X, e.Location.Y, color);
+                this.Invalidate();
+            }
         }
 
-        private void createControl(int i, int left, int top, string fileName)
+        private void FlooeFill(Bitmap bmp, int x, int y, Color fillColor)
         {
-            Bitmap image = (Bitmap)Image.FromFile(fileName);
-            this.CreateGraphics().DrawImage(image, left, top);
-            controls[i].Left = left;
-            controls[i].Top = top;
-            controls[i].Right = left + image.Width;
-            controls[i].Bottom = top + image.Height;
+            Color targetColor = bmp.GetPixel(x, y);
+            if (targetColor.ToArgb() == fillColor.ToArgb() || targetColor.ToArgb() == Color.Black.ToArgb())
+                return;
+
+            Stack<Point> pixels = new Stack<Point>();
+            pixels.Push(new Point(x, y));
+
+            while (pixels.Count > 0)
+            {
+                Point pt = pixels.Pop();
+                if (pt.X < 0 || pt.Y < 0 || pt.X >= bmp.Width || pt.Y >= bmp.Height)
+                    continue;
+
+                Color currentColor = bmp.GetPixel(pt.X, pt.Y);
+                if (currentColor.ToArgb() != targetColor.ToArgb() || currentColor.ToArgb() == Color.Black.ToArgb())
+                    continue;
+
+                bmp.SetPixel(pt.X, pt.Y, fillColor);
+
+                pixels.Push(new Point(pt.X + 1, pt.Y));
+                pixels.Push(new Point(pt.X - 1, pt.Y));
+                pixels.Push(new Point(pt.X, pt.Y + 1));
+                pixels.Push(new Point(pt.X, pt.Y - 1));
+            }
         }
 
-        private void drawFigure(object sender, PaintEventArgs e)
+        private void Painter(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
-            g.FillEllipse(Brushes.White, FIGURE_X - FIGURE_SIZE / 2, FIGURE_Y - FIGURE_SIZE / 2, FIGURE_SIZE, FIGURE_SIZE);
-            g.DrawEllipse(Pens.White, FIGURE_X - FIGURE_SIZE / 2, FIGURE_Y - FIGURE_SIZE / 2, FIGURE_SIZE, FIGURE_SIZE);
-            g.DrawLine(Pens.White, FIGURE_X - FIGURE_SIZE, FIGURE_Y - FIGURE_SIZE / 2, FIGURE_X, FIGURE_Y - FIGURE_SIZE);
-            g.DrawLine(Pens.White, FIGURE_X + FIGURE_SIZE, FIGURE_Y - FIGURE_SIZE / 2, FIGURE_X, FIGURE_Y - FIGURE_SIZE);
-            g.DrawLine(Pens.White, FIGURE_X - FIGURE_SIZE, FIGURE_Y + FIGURE_SIZE / 2, FIGURE_X, FIGURE_Y + FIGURE_SIZE);
-            g.DrawLine(Pens.White, FIGURE_X + FIGURE_SIZE, FIGURE_Y + FIGURE_SIZE / 2, FIGURE_X, FIGURE_Y + FIGURE_SIZE);
+            g.DrawImage(canvas, Point.Empty);
         }
 
-        private void fillFigure(object sender, MouseEventArgs e)
+        private void CreateButtons()
         {
-            if (e.X >= FIGURE_X - FIGURE_SIZE / 2 && e.X <= FIGURE_X + FIGURE_SIZE / 2 &&
-                e.Y >= FIGURE_Y - FIGURE_SIZE / 2 && e.Y <= FIGURE_Y + FIGURE_SIZE / 2)
+            int count = 6;
+            int posX = 0;
+
+            for (int i = 0; i < count; i++)
             {
-                this.CreateGraphics().FillEllipse(Brushes.White, FIGURE_X - FIGURE_SIZE / 2, FIGURE_Y - FIGURE_SIZE / 2, FIGURE_SIZE, FIGURE_SIZE);
+                Button button = new Button()
+                {
+                    Size = new Size(100, 50),
+                    Text = "",
+                    Location = new Point(posX, 10),
+                };
+                posX += button.Width;
+
+                if (i > 4)
+                {
+                    button.BackColor = Color.White;
+                    button.Image = new Bitmap("D:\\Veronika\\Painter\\save.bmp");
+                    button.Click += new EventHandler(OnExitClick);
+                }
+
+                else
+                {
+                    button.BackColor = colors[i];
+                }
+                button.Click += new EventHandler(SetColor);
+                this.Controls.Add(button);
             }
         }
 
-        private void setColor(object sender, MouseEventArgs e)
+        private void SetColor(object sender, EventArgs e)
         {
-            Color color = this.BackColor;
-            this.CreateGraphics().FillEllipse(new SolidBrush(color), FIGURE_X - FIGURE_SIZE / 2, FIGURE_Y - FIGURE_SIZE / 2, FIGURE_SIZE, FIGURE_SIZE);
+            Button button = sender as Button;
+            if (button != null)
+            {
+                color = button.BackColor;
+            }
         }
 
-        private void saveImage(object sender, EventArgs e)
-        {
-            Bitmap bitmap = new Bitmap(this.Width, this.Height);
-            this.CreateGraphics().DrawImage(bitmap, 0, 0);
-            bitmap.Save("output.bmp");
-        }
-
-        private void exit(object sender, EventArgs e)
+        private void OnExitClick(object sender, EventArgs e)
         {
             this.Close();
-        }
-
-        private void selectControl(object sender, MouseEventArgs e)
-        {
-            for (int i = 0; i < 5; i++)
-            {
-                if (e.X >= controls[i].Left && e.X <= controls[i].Right &&
-                    e.Y >= controls[i].Top && e.Y <= controls[i].Bottom)
-                {
-                    switch (i)
-                    {
-                        case 0:
-                            fillFigure(sender, e);
-                            break;
-                        case 1:
-                            setColor(sender, e);
-                            break;
-                        case 2:
-                            this.CreateGraphics().FillEllipse(Brushes.White, FIGURE_X - FIGURE_SIZE / 2, FIGURE_Y - FIGURE_SIZE / 2, FIGURE_SIZE, FIGURE_SIZE);
-                            break;
-                        case 3:
-                            saveImage(sender, e);
-                            break;
-                        case 4:
-                            exit(sender, e);
-                            break;
-                    }
-                }
-            }
-        }
-
-        protected override void OnMouseDown(MouseEventArgs e)
-        {
-            selectControl(this, e);
-            base.OnMouseDown(e);
         }
     }
 }
